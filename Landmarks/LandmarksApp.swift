1c9bccb3f73ea5c9fbf9f46ce3394cfd2302e13a
Pedro Borgim <pedrohenriqueborges2003@gmail.com>
2024-09-30 23:01:31 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Borgim on 30/09/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
